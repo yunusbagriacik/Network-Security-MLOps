6ec2b62d6e6b7f6f353601554986786ac8a0996e
Yunus Bagriacik <bagriacikyunus94@gmail.com>
2026-09-01 13:43:52 +0300

    data ingestion pipeline and constants have been added

---
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+"""
+defining common constant variable for training pipeline
+"""
+TARGET_COLUMN = "Result"
+PIPELINE_NAME: str = "NetworkSecurity"
+ARTIFACT_DIR: str = "Artifacts"
+FILE_NAME: str = "phisingData.csv"
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+
+SAVED_MODEL_DIR =os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
+
+
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+DATA_INGESTION_DATABASE_NAME: str = "yunusdb"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
