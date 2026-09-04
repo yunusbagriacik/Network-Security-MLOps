6d894515e498a09be5cc4f4093aeacfc0d39b2ac
Yunus Bagriacik <bagriacikyunus94@gmail.com>
2026-09-04 17:34:33 +0300

    data validation functions have been restructured

---
+++ networksecurity/components/data_validation.py
@@ -0,0 +1,41 @@
+from networksecurity.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from networksecurity.entity.config_entity import DataValidationConfig
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.constants.training_pipeline import SCHEMA_FILE_PATH
+from networksecurity.utils.main_utils.utils import read_yaml_file
+from scipy.stats import ks_2samp
+import pandas as pd
+import os, sys
+
+
+class DataValidation:
+    def __init__(self, data_ingestion_artifact: DataIngestionArtifact,
+                 data_validation_config: DataValidationConfig):
+
+        try:
+            self.data_ingestion_artifact = data_ingestion_artifact
+            self.data_validation_config = data_validation_config
+            self._schema_config = read_yaml_file(SCHEMA_FILE_PATH)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+
+    @staticmethod
+    def read_data(file_path) -> pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+
+    def initiate_data_validation(self) -> DataValidationArtifact:
+        try:
+            train_file_path = self.data_ingestion_artifact.trained_file_path
+            test_file_path = self.data_ingestion_artifact.test_file_path
+
+            ## read the data from train and test
+            train_dataframe = DataValidation.read_data(train_file_path)
+            test_dataframe = DataValidation.read_data(test_file_path)
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
---
+++ networksecurity/utils/main_utils/utils.py
@@ -0,0 +1,14 @@
+import yaml
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+import os,sys
+import numpy as np
+#import dill
+import pickle
+
+def read_yaml_file(file_path: str) -> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
