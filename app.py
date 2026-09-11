dd13deb0ad4c4ee5db7bdde58feb78834f9bf2d1
Yunus Bagriacik <bagriacikyunus94@gmail.com>
2026-09-11 20:28:54 +0300

    app.py libraries formed

--- app.py
+++ app.py
@@ -6,3 +6,17 @@ ca = certifi.where()
 
 from dotenv import load_dotenv
 load_dotenv()
+mongo_db_url = os.getenv("MONGO_DB_URL")
+print(mongo_db_url)
+
+import pymongo
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.pipeline.training_pipeline import TrainingPipeline
+
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, File, UploadFile,Request
+from uvicorn import run as app_run
+from fastapi.responses import Response
+from starlette.responses import RedirectResponse
+import pandas as pd
