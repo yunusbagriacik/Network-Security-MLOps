91a61472fe49f125c34d96a249c59a0c5bb216ea
Yunus Bagriacik <bagriacikyunus94@gmail.com>
2026-09-11 19:45:13 +0300

    training pipeline library is being formed

---
+++ networksecurity/pipeline/training_pipeline.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+
+from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
+
