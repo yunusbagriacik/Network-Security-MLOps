df33530f067b359d22108b58bc1b9e2dcc3bb381
Yunus Bagriacik <bagriacikyunus94@gmail.com>
2026-09-11 12:36:22 +0300

    mlrun happned

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -24,7 +24,8 @@ from sklearn.ensemble import (
     RandomForestClassifier,
 )
 
-
+import mlflow
+from urllib.parse import urlparse
 
 
 class ModelTrainer:
@@ -35,9 +36,9 @@ class ModelTrainer:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    """def track_mlflow(self,best_model,classificationmetric):
-        mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+    def track_mlflow(self,best_model,classificationmetric):
+       # mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
+       # tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
         with mlflow.start_run():
             f1_score=classificationmetric.f1_score
             precision_score=classificationmetric.precision_score
@@ -50,7 +51,7 @@ class ModelTrainer:
             mlflow.log_metric("recall_score",recall_score)
             mlflow.sklearn.log_model(best_model,"model")
             # Model registry does not work with file store
-            if tracking_url_type_store != "file":
+        """ if tracking_url_type_store != "file":
 
                 # Register the model
                 # There are other ways to use the Model Registry, which depends on the use case,
@@ -59,9 +60,8 @@ class ModelTrainer:
                 mlflow.sklearn.log_model(best_model, "model", registered_model_name=best_model)
             else:
                 mlflow.sklearn.log_model(best_model, "model")
-"""
 
-        
+        """
     def train_model(self, X_train, y_train, x_test, y_test):
         models = {
                 "Random Forest": RandomForestClassifier(verbose=1),
@@ -93,7 +93,7 @@ class ModelTrainer:
             "Logistic Regression":{},
             "AdaBoost":{
                 'learning_rate':[.1,.01,.001],
-                'n_estimators': [8,16,32,64,128,256]
+                'n_estimators': [4,8,16,32,64,128,256]
             }
             
         }
@@ -114,13 +114,13 @@ class ModelTrainer:
         classification_train_metric=get_classification_score(y_true=y_train, y_pred=y_train_pred)
         
         ## Track the experiements with mlflow
-        #self.track_mlflow(best_model,classification_train_metric)
+        self.track_mlflow(best_model,classification_train_metric)
 
 
         y_test_pred=best_model.predict(x_test)
         classification_test_metric=get_classification_score(y_true=y_test, y_pred=y_test_pred)
 
-        #self.track_mlflow(best_model,classification_test_metric)
+        self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
             
