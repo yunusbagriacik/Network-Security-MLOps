37056ff8d7b4285f792357551ef5f05b87429db8
Yunus Bagriacik <bagriacikyunus94@gmail.com>
2026-09-11 13:07:39 +0300

    having a look at the experiments

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -27,6 +27,10 @@ from sklearn.ensemble import (
 import mlflow
 from urllib.parse import urlparse
 
+import dagshub
+dagshub.init(repo_owner='bagriacikyunus94', repo_name='Network-Security-MLOps', mlflow=True)
+
+
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
