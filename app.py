c70ff8300d637b6e8b7181a51b23a49e8567dac4
Yunus Bagriacik <bagriacikyunus94@gmail.com>
2026-09-11 21:48:46 +0300

    batch prediction has been made

--- app.py
+++ app.py
@@ -78,7 +78,11 @@ async def predict_route(request: Request,file: UploadFile = File(...)):
         df.to_csv('prediction_output/output.csv')
         table_html = df.to_html(classes='table table-striped')
         #print(table_html)
-        return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
+        return templates.TemplateResponse(
+            request=request,
+            name="table.html",
+            context={"table": table_html}
+        )
         
     except Exception as e:
             raise NetworkSecurityException(e,sys)
